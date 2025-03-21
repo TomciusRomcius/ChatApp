ec83a2f8a413ab62d4c0fd72b5094ef21cca30a9
Tomas <tomas.ew88@gmail.com>
2025-03-21 15:08:42 +0200

    Send user messages working!!

--- chatapp.web/src/app/application/_components/_chat/chatWindow.tsx
+++ chatapp.web/src/app/application/_components/_chat/chatWindow.tsx
@@ -1,12 +1,33 @@
 import { CurrentChatContext } from "@/context/currentChatContext";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import TextMessage from "@/types";
 import UserMessagingService from "@/services/userMessagingService";
 import Message from "./message";
+import CurrentUserContext from "@/context/currentUserContext";
 
 export default function ChatWindow() {
+    const { currentUser } = useContext(CurrentUserContext);
     const { currentChat } = useContext(CurrentChatContext);
     const [messages, setMessages] = useState<TextMessage[]>([]);
+    const sendMessageRef = useRef<HTMLInputElement>(null);
+
+    const onSendMessage = () => {
+        const content = sendMessageRef.current?.value;
+
+        if (currentChat?.id && content) {
+            UserMessagingService.SendMessageToFriend(currentChat.id, content);
+            // TODO: set createdAt
+            setMessages([
+                ...messages,
+                {
+                    senderId: currentUser.id,
+                    receiverId: currentChat.id,
+                    content: content,
+                    createdAt: null,
+                },
+            ]);
+        }
+    };
 
     useEffect(() => {
         if (currentChat) {
@@ -17,16 +38,28 @@ export default function ChatWindow() {
             );
         }
     }, [currentChat]);
-    console.log(messages);
 
     return (
-        <div className="px-64 py-8 col-span-5 row-span flex flex-col gap-4">
-            {messages.map((textMessage) => (
-                <Message
-                    key={Math.random() * 100}
-                    message={textMessage}
-                ></Message>
-            ))}
+        <div className="w-full h-screen px-64 py-8 col-span-5">
+            <div className="w-full h-full grid rows-10">
+                <div className="flex h-full overflow-y-auto flex-col gap-4 row-span-9">
+                    {messages.map((textMessage) => (
+                        <Message
+                            key={Math.random() * 100}
+                            message={textMessage}
+                        ></Message>
+                    ))}
+                </div>
+
+                <div className="w-full px-4 py-2 flex items-center justify-between bg-gray-200">
+                    <input
+                        ref={sendMessageRef}
+                        className="h-full"
+                        placeholder="Send message"
+                    />
+                    <button onClick={onSendMessage}>Send</button>
+                </div>
+            </div>
         </div>
     );
 }
