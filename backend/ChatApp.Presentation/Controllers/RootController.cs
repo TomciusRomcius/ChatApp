7bc3c30bcfc0c33041f09dfb3bd5e5e2851705be
Tomas <tomas.ew88@gmail.com>
2025-04-12 14:22:06 +0300

    GET at / for checking if api is alive

---
+++ backend/ChatApp.Presentation/Controllers/RootController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Presentation.Controllers;
+
+[ApiController]
+[Route("/")]
+public class RootController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("API Working!");
+    }
+}
