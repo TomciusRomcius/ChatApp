1a2715c8329aa4b71b3ef6d1e95e7b46789de7b9
Tomas <tomas.ew88@gmail.com>
2025-04-19 09:55:33 +0300

    Util function for getting current user id

--- backend/ChatApp.Presentation/Utils/ControllerUtils.cs
+++ backend/ChatApp.Presentation/Utils/ControllerUtils.cs
@@ -1,4 +1,5 @@
-﻿using ChatApp.Domain.Utils;
+﻿using System.Security.Claims;
+using ChatApp.Domain.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatApp.Presentation.Utils;
@@ -13,5 +14,10 @@ public static class ControllerUtils
             ResultErrorType.FORBIDDEN_ERROR => new ForbidResult(),
             _ => new StatusCodeResult(StatusCodes.Status500InternalServerError)
         };
+    }
+
+    public static string? GetCurrentUserId(HttpContext httpContext)
+    {
+        return httpContext.User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value;
     }
 }
