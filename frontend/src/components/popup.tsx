95c6dd05392d6f2b0c74e3479c78f771ae80a397
Tomas <tomas.ew88@gmail.com>
2025-04-22 09:41:21 +0300

    Fix event propagation

--- frontend/src/components/popup.tsx
+++ frontend/src/components/popup.tsx
@@ -7,8 +7,17 @@ interface PopupProps {
 }
 
 export default function Popup(props: PopupProps) {
+    let outsideContainerRef = useRef<HTMLDivElement | null>(null);
     let containerRef = useRef<HTMLDivElement | null>(null);
-    
+
+    const onOutsideClick = (e: MouseEvent) => {
+        props.onClose();
+    }
+
+    const onContainerMouseDown = (e: MouseEvent) => {
+        e.stopPropagation();
+    }
+
     useEffect(() => {
         const onKeyDown = (ev: KeyboardEvent) => {
             if (ev.key == "Escape") {
@@ -17,13 +26,7 @@ export default function Popup(props: PopupProps) {
             }
         };
         
-        const onContainerMouseDown = (e: MouseEvent) => {
-            props.onClose();
-
-        }
-
         window.addEventListener("keydown", onKeyDown);
-        containerRef.current!.addEventListener("click", onContainerMouseDown);
 
         return () => {
             window.removeEventListener("keydown", onKeyDown);
@@ -31,9 +34,11 @@ export default function Popup(props: PopupProps) {
     }, [props]);
 
     return (
-        <div ref={containerRef} className="fixed flex h-screen w-screen items-center justify-center">
+        <div onClick={onOutsideClick} ref={outsideContainerRef} className="fixed flex h-screen w-screen items-center justify-center">
             <div className="absolute left-0 top-0 h-full w-full bg-background-popup opacity-75"></div>
             <div
+                onClick={onContainerMouseDown}
+                ref={containerRef}
                 className={`z-10 rounded-sm bg-background-0 p-4 ${props.className}`}
             >
                 {props.children}
