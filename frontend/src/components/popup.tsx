be8617e9f59d6818e485681756a44fdc772b409d
Tomas <tomas.ew88@gmail.com>
2025-04-22 09:16:06 +0300

    Close popup when clicking outside

--- frontend/src/components/popup.tsx
+++ frontend/src/components/popup.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 interface PopupProps {
     children: ReactNode;
@@ -7,6 +7,8 @@ interface PopupProps {
 }
 
 export default function Popup(props: PopupProps) {
+    let containerRef = useRef<HTMLDivElement | null>(null);
+    
     useEffect(() => {
         const onKeyDown = (ev: KeyboardEvent) => {
             if (ev.key == "Escape") {
@@ -14,8 +16,14 @@ export default function Popup(props: PopupProps) {
                 window.removeEventListener("keydown", onKeyDown);
             }
         };
+        
+        const onContainerMouseDown = (e: MouseEvent) => {
+            props.onClose();
+
+        }
 
         window.addEventListener("keydown", onKeyDown);
+        containerRef.current!.addEventListener("click", onContainerMouseDown);
 
         return () => {
             window.removeEventListener("keydown", onKeyDown);
@@ -23,7 +31,7 @@ export default function Popup(props: PopupProps) {
     }, [props]);
 
     return (
-        <div className="fixed flex h-screen w-screen items-center justify-center">
+        <div ref={containerRef} className="fixed flex h-screen w-screen items-center justify-center">
             <div className="absolute left-0 top-0 h-full w-full bg-background-popup opacity-75"></div>
             <div
                 className={`z-10 rounded-sm bg-background-0 p-4 ${props.className}`}
