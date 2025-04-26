12f258b11b9df450e7b20aa37ffa001787d31f9e
Tomas <tomas.ew88@gmail.com>
2025-04-26 10:37:51 +0300

    Make user's username unique

---
+++ backend/ChatApp.Application/Migrations/20250425064316_Set username as unique.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Setusernameasunique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "PublicUserInfos",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicUserInfos_Username",
+                table: "PublicUserInfos",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PublicUserInfos_Username",
+                table: "PublicUserInfos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "PublicUserInfos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
