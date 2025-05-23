3d369982ad32e26d591a2c98b8e00bd690f25752
Tomas <tomas.ew88@gmail.com>
2025-05-23 12:10:35 +0300

    Display errors to client

--- frontend/src/app/auth/account-setup/page.tsx
+++ frontend/src/app/auth/account-setup/page.tsx
@@ -3,19 +3,33 @@
 import AuthFlowLayout from "@/app/auth/_components/authFlowLayout";
 import Input from "@/components/Input";
 import ButtonWithPassword from "@/app/auth/_components/buttonWithPassword";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import UserService from "@/services/userService";
 import { useRouter } from "next/navigation";
+import ErrorMessage from "@/components/errorMessage";
 
 export default function Page() {
     const router = useRouter();
+    const [error, setError] = useState<string>("");
+    
     const handleSetup = (ev: FormEvent) => {
         ev.preventDefault();
 
         const form = ev.target as HTMLFormElement;
         const username = form.username.value;
 
-        if (!username) return;
+        if (!username) {
+            return;
+        }
+        
+        if (username.length < 2) {
+            setError("Username must be at least 3 characters long");
+            return;
+        }
+        if (username.length > 20) {
+            setError("Username length must not exceed 20 characters");
+            return;
+        }
 
         UserService.SetUserInfo({
             username: username,
@@ -41,6 +55,7 @@ export default function Page() {
                 value="Setup"
                 className="cursor-pointer rounded-md bg-accent p-2"
             />
+            <ErrorMessage message={error}/>
         </AuthFlowLayout>
     );
 }
